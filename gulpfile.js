803453ae41f77bc2ec2d305cfc648d934b2f4811
炒饭 <chaofan@2dfire.com>
2017-07-20 17:29:31 +0800

    踩踩踩

--- gulpfile.js
+++ gulpfile.js
@@ -3,21 +3,27 @@ const git = require('gulp-git');
 const path = require('path');
 const eslint = require('gulp-eslint');
 const debug = require('gulp-debug');
+const config = {
+  source: '',
+  target: 'master',
+
+};
 
 gulp.task('diff', function(){
     const src = path.resolve(process.cwd(), '**');
     return gulp.src(src)
-        // .pipe(git.diff('master'))
-        // .pipe(debug({ title: 'diff:' }))
-        .pipe(eslint({
-            useEslintrc: false,
-            configFile: path.resolve(__dirname, '.eslintrc.js')
-        }))
-        .pipe(eslint.format())
-        .pipe(eslint.results(results => {
-            // Called once for all ESLint results.
-            console.log(`Total Results: ${results.length}`);
-            console.log(`Total Warnings: ${results.warningCount}`);
-            console.log(`Total Errors: ${results.errorCount}`);
-        }));
+        .pipe(git.diff('master', {log: true}))
+        .pipe(gulp.dest(path.resolve(__dirname, './diff.out')));
+        // // .pipe(debug({ title: 'diff:' }))
+        // .pipe(eslint({
+        //     useEslintrc: false,
+        //     configFile: path.resolve(__dirname, '.eslintrc.js')
+        // }))
+        // .pipe(eslint.format())
+        // .pipe(eslint.results(results => {
+        //     // Called once for all ESLint results.
+        //     console.log(`Total Results: ${results.length}`);
+        //     console.log(`Total Warnings: ${results.warningCount}`);
+        //     console.log(`Total Errors: ${results.errorCount}`);
+        // }));
 });
